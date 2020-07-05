30dee063179eae20d0413a151219d5251940204c
Barbosa <mbo2@cin.ufpe.br>
2020-07-05 18:23:57 -0300

    add new route and navigation

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/routes';
 
-const App: () => React$Node = () => {
+const App = () => {
   return <Routes />;
 };
 
