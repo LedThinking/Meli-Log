9cf0b1fc0951502442082380d3d8d49d01719adc
Barbosa <mbo2@cin.ufpe.br>
2020-07-05 18:25:00 -0300

    add register screen

---
+++ src/screens/Register/styles.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 0,
+  },
+  fields: {
+    justifyContent: 'center',
+    width: 370,
+    height: 50,
+    marginTop: 10,
+  },
+  button: {
+    marginTop: 10,
+    width: 377,
+    height: 55,
+    alignItems: 'center',
+  },
+  card: {width: 370, marginTop: 10},
+});
+
+export default style;
